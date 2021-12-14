c10493e01d9fdaac92e3ce771b6971054a1953a1
volk22kamin <69310378+volk22kamin@users.noreply.github.com>
2021-12-14 11:53:34 +0200

    basicly finished but can improve, just need to cool down the 50 fetches

---
+++ js/script.js
@@ -0,0 +1,86 @@
+const imageContainer = document.getElementById('imageContainer');
+const loader  = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photoArray = [];
+
+// unsplash API
+const count = 30;
+const apiKey = '-Urt1ba6H_QJ925MCQkfn-aYtl-19FL9wvg3-ZYvHu8';
+const APIurl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+
+function imageLoaded(){
+    imagesLoaded++;
+    console.log('loaded');
+    if (imagesLoaded === totalImages){
+        ready = true;
+    }
+}
+
+// helper function to set the attributes to the element 
+// as an object
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+      element.setAttribute(key, attributes[key]);
+    }
+  }
+
+function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photoArray.length;
+    photoArray.forEach((photo) => {
+        // creating href element
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        //creatimg img element
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src:photo.urls.regular,
+            alt: photo.description,
+        });
+        // if the description is null
+        if(!photo.description){
+            img.setAttribute('title', 'unsplash photo');
+        } else {
+            img.setAttribute('title', photo.description);
+        }
+        // event listener check when eah is finished loading 
+        img.addEventListener('load', imageLoaded);
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    })
+}
+
+
+async function getPhotosFromAPI(){
+    try {
+        const response = await fetch(APIurl);
+        photoArray = await response.json();
+        displayPhotos();
+        loader.hidden = true;
+    } catch(error){
+        // catch the error
+    }
+}
+
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotosFromAPI();
+    }
+})
+
+
+
+
+// on load
+getPhotosFromAPI();
